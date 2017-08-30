664d358213a1cc4c81d4702cd006513c76ff1aa6
Arc <1070711@gmail.com>
2017-08-30 11:11:18 -0600

    исправленные пробелы 3

--- gs.ex
+++ gs.ex
@@ -4,7 +4,7 @@ defmodule gs do
 
 
   def start_link do
-	{:ok, pid} = GenServer.start_link(gs, [], [{:gs, __MODULE__}])
+    {:ok, pid} = GenServer.start_link(gs, [], [{:gs, __MODULE__}])
   end
 
 
@@ -13,7 +13,7 @@ defmodule gs do
   end
   
   def read(Name) do
-	GenServer.call(pid, {:read , Name})
+    GenServer.call(pid, {:read , Name})
   end
 
   def close() do
@@ -24,46 +24,46 @@ defmodule gs do
 
 
   def init([]) do
-	{:ok, :dets.open_file(:md, [type: :set])}
+    {:ok, :dets.open_file(:md, [type: :set])}
   end
 
 
   def handle_call({:create, Name, Adress, Bissnes}, _from, state) do
-	case :dets.lookup(md, Name)  do
-	[] ->
-	:dets.insert(md, {Name, Adress,Bissnes})
-	{reply, {oke}, State}
-	_else -> 
-	{:reply, {error}, State}
+    case :dets.lookup(md, Name)  do
+    [] ->
+    :dets.insert(md, {Name, Adress,Bissnes})
+    {reply, {oke}, State}
+    _else -> 
+    {:reply, {error}, State}
     end
   end
  
   def handle_call({:read , Name}, _from, state) do
-	case :dets.lookup(md, Name)  do
-	[] ->
-	:dets.lookup(md, Name)
-	{:reply, {error}, State}
-	_else -> 
-	{:reply, :dets.lookup(md, Name), State}
-	end
+    case :dets.lookup(md, Name)  do
+    [] ->
+    :dets.lookup(md, Name)
+    {:reply, {error}, State}
+    _else -> 
+    {:reply, :dets.lookup(md, Name), State}
+    end
   end
 
   def handle_call({close}, _From, State) do 
-	:dets.close(md)	
-	{:reply, {okeKlose}, State}
+    :dets.close(md)	
+    {:reply, {okeKlose}, State}
   end
 
 
   def handle_info(_info, state) do
-	{:noreply, state}
+    {:noreply, state}
   end
 
   def terminate(_reason, _state) do
-	{:ok}
+    {:ok}
   end
 
   def code_change(_old_version, state, _extra) do
-	{:ok, state}
+    {:ok, state}
   end
 
 end
